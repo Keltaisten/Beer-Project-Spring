d1b8b02f4a8230606235d6c4a614c60e5ad5f7eb
Keltaisten <keltaisten@gmail.com>
2022-05-20 12:04:19 +0200

    Initial commit

---
+++ src/main/java/beerprojectspring/BeerProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package beerprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerProjectSpringApplication.class, args);
+    }
+
+}
